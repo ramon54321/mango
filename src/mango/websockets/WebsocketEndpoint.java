65619a95b28469b49c413bf4017de1294d91a2ae
Anton <anton5@users.noreply.github.com>
2017-03-07 21:03:45 +0200

    Formatted.

--- src/mango/websockets/WebsocketEndpoint.java
+++ src/mango/websockets/WebsocketEndpoint.java
@@ -8,40 +8,40 @@ import java.util.ArrayList;
 @ServerEndpoint("/websocket")
 public class WebsocketEndpoint {
 
-  private static List<Session> userSessions = new ArrayList<Session>();
+	private static List<Session> userSessions = new ArrayList<Session>();
 
-  private static void addSession(Session session){
-    userSessions.add(session);
-  }
+	private static void addSession(Session session) {
+		userSessions.add(session);
+	}
 
-  private static void removeSession(Session session){
-    userSessions.remove(session);
-  }
+	private static void removeSession(Session session) {
+		userSessions.remove(session);
+	}
 
-  public static void broadcast(){
-    for(Session s : userSessions) {
-      try{
-        s.getBasicRemote().sendText("update_needed");
-      } catch (Exception e){
-        e.printStackTrace();
-      }
-    }
-  }
+	public static void broadcast() {
+		for (Session s : userSessions) {
+			try {
+				s.getBasicRemote().sendText("update_needed");
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
 
-  @OnOpen
-  public void onOpen(Session session){
-    System.out.println("Connection opened.");
-    addSession(session);
-  }
+	@OnOpen
+	public void onOpen(Session session) {
+		System.out.println("Connection opened.");
+		addSession(session);
+	}
 
-  @OnClose
-  public void onClose(Session session){
-    System.out.println("Connection closed.");
-    removeSession(session);
-  }
+	@OnClose
+	public void onClose(Session session) {
+		System.out.println("Connection closed.");
+		removeSession(session);
+	}
 
-  @OnMessage
-  public void onMessage(String message, Session session){
-    System.out.println("Message: " + message);
-  }
+	@OnMessage
+	public void onMessage(String message, Session session) {
+		System.out.println("Message: " + message);
+	}
 }
